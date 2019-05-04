ffc155f5713cd3dfc5a6c1c12aa4b180a72e183b
ntrv <hydrangea56@me.com>
2019-05-05 00:27:13 +0900

    Implements Pimple\ServiceProviderInterface

--- app/Services/Sample/HelloServiceProvider.php
+++ app/Services/Sample/HelloServiceProvider.php
@@ -1,13 +1,12 @@
 <?php
 namespace App\Services\Sample;
 
+use Pimple\Container;
 use Pimple\ServiceProviderInterface;
-
-use Psr\Container\ContainerInterface;
 
 class HelloServiceProvider implements ServiceProviderInterface
 {
-    public function register(ContainerInterface $c)
+    public function register(Container $c)
     {
         //
     }
