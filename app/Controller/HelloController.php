3dce3822a1d5a9c0fee86404df2f2324bcc48c4b
ntrv <hydrangea56@me.com>
2019-05-04 21:41:04 +0900

    Duplicate

--- app/Controller/HelloController.php
+++ app/Controller/HelloController.php
@@ -23,6 +23,15 @@ class HelloController extends Controller
     {
         $hello = new Hello;
         $name = $args['name'];
+        return $this->view->render($response, 'hello.twig', [
+            'message' => $hello->withName($name)
+        ]);
+    }
+
+    public function text(Request $request, Response $response, array $args): Response
+    {
+        $hello = new Hello;
+        $name = $args['name'];
         $response->getBody()->write($hello->withName($name) . "\n");
         return $response;
     }
