7e0aca8135ee380fe0cc798f553afdb5c5a80347
ntrv <hydrangea56@me.com>
2019-05-04 21:40:16 +0900

    Disable template caching

--- app/configs.php
+++ app/configs.php
@@ -3,7 +3,7 @@ return [
     'view' => [
         'template_path' => __DIR__ . '/../resources/views',
         'twig' => [
-            'cache' => __DIR__ . '/../cache/view'
+            // 'cache' => __DIR__ . '/../cache/view'
         ]
     ]
 ];
