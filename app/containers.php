45e5bc5c1497fb74b32354eb48b1a6ddbbab2891
ntrv <hydrangea56@me.com>
2019-05-04 20:49:07 +0900

    Remove

--- app/containers.php
+++ app/containers.php
@@ -1,8 +1,6 @@
 <?php
 use \Psr\Container\ContainerInterface;
 
-use App\Model\Hello;
-
 /** @var ContainerInterface */
 $container = $app->getContainer();
 
@@ -12,8 +10,4 @@ $container['view'] = function (ContainerInterface $container) use ($configs) {
 
     $view = new \Slim\Views\Twig($config['template_path'], $config['twig']);
     return $view;
-};
-
-$container['model.hello'] = function (ContainerInterface $container) {
-    return new Hello;
 };
