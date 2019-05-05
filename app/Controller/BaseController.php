908f0ff88153bcdee1e8d1ab34b697a929c0987b
ntrv <hydrangea56@me.com>
2019-05-06 06:34:32 +0900

    Pass Capsule

--- app/Controller/BaseController.php
+++ app/Controller/BaseController.php
@@ -5,14 +5,21 @@ use \Psr\Container\ContainerInterface;
 use \Psr\Log\LoggerInterface;
 
 use App\Services\LoggerServiceProvider;
+use App\Services\Database\EloquentServiceProvider;
+
+use \Illuminate\Database\Capsule\Manager as Capsule;
 
 abstract class BaseController
 {
     /** @var LoggerInterface */
     protected $logger;
 
+    /** @var Capsule */
+    protected $db;
+
     public function __construct(ContainerInterface $c)
     {
         $this->logger = $c->get(LoggerServiceProvider::class);
+        $this->db = $c->get(EloquentServiceProvider::class);
     }
 }
