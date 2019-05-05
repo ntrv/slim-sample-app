bdb801e6b2e7d0e9110e84a55e22ab9387a95798
ntrv <hydrangea56@me.com>
2019-05-05 19:45:29 +0900

    Create HtmlController

---
+++ app/Controller/HtmlController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use \Psr\Container\ContainerInterface;
+use \Slim\Views\Twig;
+
+abstract class HtmlController extends BaseController
+{
+    /** @var Twig */
+    protected $view;
+
+    public function __construct(ContainerInterface $c)
+    {
+        parent::__construct($c);
+        $this->view = $this->container->get('view');
+    }
+}
--- app/Controller/Sample/HelloController.php
+++ app/Controller/Sample/HelloController.php
@@ -4,27 +4,21 @@ namespace App\Controller\Sample;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-use App\Controller\BaseController;
+use App\Controller\HtmlController;
 
 use \Psr\Container\ContainerInterface;
-
-use \Slim\Views\Twig;
 
 use App\Model\HelloInterface;
 use App\Model\Hello;
 
-class HelloController extends BaseController
+class HelloController extends HtmlController
 {
     /** @var HelloInterface */
     private $hello;
 
-    /** @var Twig */
-    private $view;
-
     public function __construct(ContainerInterface $c)
     {
         parent::__construct($c);
-        $this->view = $this->container->get('view');
     }
 
     public function withHtml(Request $request, Response $response, array $args): Response
